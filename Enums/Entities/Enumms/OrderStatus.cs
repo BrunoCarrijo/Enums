718b0488bf4b1b80520a5ca065353200851a58cb
Bruno Carrijo <bruno.carrijo1009@gmail.com>
2021-11-03 21:19:41 -0300

    Projeto com a constante tipificada ENUM

---
+++ Enums/Entities/Enumms/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enums.Entities.Enumms
+{
+    internal enum OrderStatus : int
+    {
+        PendingPayment =0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Enums.Entities;
+using Enums.Entities.Enumms;
+
+//     Enumerações
+//• É um tipo especial que serve para especificar de forma literal um conjunto
+//de constantes relacionadas
+//• Palavra chave em C#: enum
+//• Nota: enum é um tipo valor
+//• Vantagem: melhor semântica, código mais legível e auxiliado pelo
+//compilador
+namespace Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
